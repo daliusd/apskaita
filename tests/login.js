43c226811634db11369d53e07de573aa52b059f6
Dalius Dobravolskas <dalius.dobravolskas@gmail.com>
2020-10-18 17:50:42 +0300

    Pataisyti testai.

--- tests/login.js
+++ tests/login.js
@@ -1,5 +1,5 @@
 const { getDocument, queries } = require('pptr-testing-library');
-const chance = require('chance');
+const chance = require('chance').Chance();
 
 const { getByLabelText, getByText } = queries;
 
