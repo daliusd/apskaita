3210f83b584ce6d4bbe25f3af7c0910547226ff2
Dalius Dobravolskas <dalius.dobravolskas@gmail.com>
2021-01-12 01:20:14 +0200

    Simple index page added.

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,31 @@ export default function Index() {
   const [session] = useSession();
 
   if (!session) {
-    return null;
+    return (
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          Individualios veiklos apskaita.
+        </Grid>
+        <Grid item xs={12}>
+          Paprastai ir greitai galite padaryti šiuos dalykus:
+          <ul>
+            <li> Išrašyti sąskaitą </li>
+            <li> Sugeneruoti PDF sąskaitas faktūras </li>
+          </ul>
+          Jums tereikia prisijungti su savo Google paskyra.
+        </Grid>
+        <Grid item xs={12}>
+          Ateityje galėsite:
+          <ul>
+            <li> Išrašyti išankstines sąskaitas faktūras </li>
+            <li> Sekti išlaidas </li>
+            <li> Automatiškai suskaičiuoti mokesčius </li>
+            <li> Peržiūrėti apskaitos žurnalą </li>
+            <li> ... ir daugiau. </li>
+          </ul>
+        </Grid>
+      </Grid>
+    );
   }
 
   return (
