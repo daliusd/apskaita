78feba64f91046c2f713d1c53266d225539b6d6d
Dalius Dobravolskas <dalius.dobravolskas@gmail.com>
2021-01-30 19:14:14 +0200

    Testas su keliomis paslaugomis.

---
+++ tests/invoice-multiple-line-items.test.ts
@@ -0,0 +1,48 @@
+import { login } from './login';
+import { fillNewInvoice, validateInvoice } from './invoices';
+
+import { IInvoice } from '../db/db';
+
+describe('Settings test', () => {
+  beforeAll(async () => {
+    await page.goto('http://localhost:4000');
+  });
+
+  it('should create invoice', async () => {
+    await login(page);
+
+    const invoice: IInvoice = {
+      seriesName: 'TEST',
+      seriesId: 0,
+      created: new Date(2020, 0, 31).getTime(),
+      price: 100,
+      buyer: 'Dalius',
+      seller: 'Jonas',
+      issuer: 'Jonas',
+      extra: 'Apmokėti per 10 dienų',
+      lineItems: [
+        { name: 'Konsultacija', unit: 'val.', amount: 2, price: 25 },
+        { name: 'Testavimas', unit: 'val.', amount: 2, price: 25 },
+      ],
+    };
+
+    await page.click('text="Nauja sąskaita faktūra"');
+    await page.waitForNavigation({
+      url: 'http://localhost:4000/saskaitos/nauja',
+    });
+
+    await fillNewInvoice(page, invoice);
+
+    await page.click('text="Sukurti"');
+
+    await page.waitForNavigation();
+    expect(
+      page.url().startsWith('http://localhost:4000/saskaitos/id/'),
+    ).toBeTruthy();
+
+    await page.waitForSelector('text="Sąskaita faktūra sukurta"');
+
+    invoice.seriesId = 1; // new series starts with 1
+    await validateInvoice(page, invoice);
+  });
+});
