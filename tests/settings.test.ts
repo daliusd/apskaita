c9f18184e250c9f9f9b996073a4da426338e8476
Dalius Dobravolskas <dalius.dobravolskas@gmail.com>
2021-02-01 20:31:15 +0200

    Nustatymų testas perrašytas.

--- tests/settings.test.ts
+++ tests/settings.test.ts
@@ -1,5 +1,6 @@
 import { login } from './login';
 import { validateTextArea, validateInput } from './utils';
+import { setSeller, setIssuer, setExtra, setZeroes } from './settings';
 
 describe('Settings test', () => {
   beforeAll(async () => {
@@ -10,47 +11,12 @@ describe('Settings test', () => {
     await login(page);
 
     await page.click('text="Nustatymai"');
-    await page.waitForNavigation(/*{ url: 'http://localhost:3000/nustatymai' }*/);
+    await page.waitForNavigation({ url: 'http://localhost:4000/nustatymai' });
 
-    await page.click(
-      'textarea[aria-label="Tavo rekvizitai sąskaitai faktūrai"]',
-    );
-    await page.fill(
-      'textarea[aria-label="Tavo rekvizitai sąskaitai faktūrai"]',
-      'mano rekvizitai',
-    );
-    await page.click('text="Išsaugoti rekvizitus"');
-
-    await page.click(
-      'input[aria-label="Asmuo įprastai išrašantis sąskaitas faktūras"]',
-    );
-    await page.fill(
-      'input[aria-label="Asmuo įprastai išrašantis sąskaitas faktūras"]',
-      'vardenis pavardenis',
-    );
-    await page.click('text="Išsaugoti"');
-
-    await page.click(
-      'textarea[aria-label="Papildoma informacija sąskaitoje faktūroje"]',
-    );
-    await page.fill(
-      'textarea[aria-label="Papildoma informacija sąskaitoje faktūroje"]',
-      'papildoma informacija',
-    );
-    await page.click(
-      'button[aria-label="Išsaugoti papildomą informaciją"] >> text="Išsaugoti"',
-    );
-
-    await page.click(
-      'input[aria-label="Skaitmenų skaičius sąskaitos faktūros serijos numeryje"]',
-    );
-    await page.fill(
-      'input[aria-label="Skaitmenų skaičius sąskaitos faktūros serijos numeryje"]',
-      '3',
-    );
-    await page.click(
-      'button[aria-label="Išsaugoti skaitmenų skaičių"] >> text="Išsaugoti"',
-    );
+    await setSeller(page, 'mano rekvizitai');
+    await setIssuer(page, 'vardenis pavardenis');
+    await setExtra(page, 'papildoma informacija');
+    await setZeroes(page, '3');
 
     await page.goto('http://localhost:4000/nustatymai');
 
---
+++ tests/settings.ts
@@ -0,0 +1,47 @@
+import { Page } from 'playwright';
+
+export async function setSeller(page: Page, seller: string) {
+  await page.click('textarea[aria-label="Tavo rekvizitai sąskaitai faktūrai"]');
+  await page.fill(
+    'textarea[aria-label="Tavo rekvizitai sąskaitai faktūrai"]',
+    seller,
+  );
+  await page.click('text="Išsaugoti rekvizitus"');
+}
+
+export async function setIssuer(page: Page, issuer: string) {
+  await page.click(
+    'input[aria-label="Asmuo įprastai išrašantis sąskaitas faktūras"]',
+  );
+  await page.fill(
+    'input[aria-label="Asmuo įprastai išrašantis sąskaitas faktūras"]',
+    issuer,
+  );
+  await page.click('text="Išsaugoti"');
+}
+
+export async function setExtra(page: Page, extra: string) {
+  await page.click(
+    'textarea[aria-label="Papildoma informacija sąskaitoje faktūroje"]',
+  );
+  await page.fill(
+    'textarea[aria-label="Papildoma informacija sąskaitoje faktūroje"]',
+    extra,
+  );
+  await page.click(
+    'button[aria-label="Išsaugoti papildomą informaciją"] >> text="Išsaugoti"',
+  );
+}
+
+export async function setZeroes(page: Page, zeroes: string) {
+  await page.click(
+    'input[aria-label="Skaitmenų skaičius sąskaitos faktūros serijos numeryje"]',
+  );
+  await page.fill(
+    'input[aria-label="Skaitmenų skaičius sąskaitos faktūros serijos numeryje"]',
+    zeroes,
+  );
+  await page.click(
+    'button[aria-label="Išsaugoti skaitmenų skaičių"] >> text="Išsaugoti"',
+  );
+}
