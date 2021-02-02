cafe8570053c7670f3acef02448ab265da852294
Dalius Dobravolskas <daliusd@wix.com>
2021-02-03 01:20:29 +0200

    Logo palaikymas (#131)
    closes #115

---
+++ components/LogoEdit.tsx
@@ -0,0 +1,111 @@
+import React, { useState, useEffect, useContext } from 'react';
+import Button from '@material-ui/core/Button';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import ImageIcon from '@material-ui/icons/Image';
+import DeleteIcon from '@material-ui/icons/Delete';
+import useSWR, { mutate } from 'swr';
+
+import { IContext, Context } from '../src/Store';
+
+export default function ZeroesEdit() {
+  const { dispatch } = useContext<IContext>(Context);
+
+  const { data, error } = useSWR('/api/settings/logo');
+
+  const handleChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const data = new FormData();
+    data.append('logo', event.target.files[0]);
+
+    event.target.value = null; // reset
+
+    const resp = await fetch('/api/logo', {
+      method: 'POST',
+      body: data,
+    });
+
+    if (!resp.ok || !(await resp.json()).success) {
+      dispatch({
+        type: 'SET_MESSAGE',
+        text: 'Nepavyko pakeisti logo.',
+        severity: 'error',
+      });
+      return;
+    }
+
+    dispatch({
+      type: 'SET_MESSAGE',
+      text: 'Logo pakeistas.',
+      severity: 'success',
+    });
+
+    mutate('/api/settings/logo');
+  };
+
+  const handleDelete = async () => {
+    await fetch('/api/settings/logo', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ value: '' }),
+    });
+
+    mutate('/api/settings/logo');
+  };
+
+  const loaded = data || error;
+
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <Typography variant="h6" component="h1" noWrap>
+          Logo
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Typography variant="body1" component="div">
+          Galite pridėti/pakeisti logotipą, kuris bus naudojamas sąskaitos
+          faktūros PDF faile.
+        </Typography>
+      </Grid>
+      {!loaded && <CircularProgress />}
+      {loaded && (
+        <>
+          {data && data.value && (
+            <Grid item xs={12}>
+              <img src={data.value} alt="logo" />
+            </Grid>
+          )}
+          <Grid item xs={12}>
+            <Button
+              startIcon={<ImageIcon />}
+              color="primary"
+              component="label"
+              aria-label="Pakeisti logo"
+            >
+              Pakeisti logo
+              <input
+                type="file"
+                accept="image/*"
+                hidden
+                onChange={handleChange}
+              />
+            </Button>
+            {data && data.value && (
+              <Button
+                startIcon={<DeleteIcon />}
+                color="secondary"
+                aria-label="Pašalinti logo"
+                onClick={handleDelete}
+              >
+                Pašalinti logo
+              </Button>
+            )}
+          </Grid>
+        </>
+      )}
+    </Grid>
+  );
+}
