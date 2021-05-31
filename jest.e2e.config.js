5efef19032acb0d0637ab5a2feca08ace768988b
Dalius Dobravolskas <dalius.dobravolskas@gmail.com>
2021-05-31 21:38:09 +0300

    package.json atnaujinimas.

--- jest.e2e.config.js
+++ jest.e2e.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'jest-playwright-preset',
   testMatch: ['<rootDir>/tests/*.test.js', '<rootDir>/tests/*.test.ts'],
-  testTimeout: 30000,
+  testTimeout: 60000,
   verbose: true,
   transform: {
     '\\.ts$': ['babel-jest', { configFile: './babel.config.test.js' }],
