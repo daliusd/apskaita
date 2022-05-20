219e837bde01c0f971c8fd34a19799b2615f7dab
Dalius Dobravolskas <dalius.dobravolskas@gmail.com>
2022-05-20 23:31:27 +0300

    Timeout increased even more.

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,6 @@
 import { PlaywrightTestConfig } from '@playwright/test';
 const config: PlaywrightTestConfig = {
-  timeout: 60000,
+  timeout: 120000,
   testDir: 'tests',
   use: {
     headless: true,
