210a6aa2886acc6e7ef8f415406a330dffb665ad
Dalius Dobravolskas <dalius.dobravolskas@gmail.com>
2023-04-06 21:51:10 +0300

    Minor bug fix.

--- pages/api/uniqueseriesnames/index.ts
+++ pages/api/uniqueseriesnames/index.ts
@@ -7,9 +7,10 @@ import { errorHandler } from '../../../utils/report-mailer';
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   return dbWrapper(req, res, async (db) => {
     const invoiceType =
-      typeof req.query.invoiceType === 'string'
+      req.query.invoiceType &&
+      (typeof req.query.invoiceType === 'string'
         ? req.query.invoiceType
-        : req.query.invoiceType[0];
+        : req.query.invoiceType[0]);
     const seriesNames = await getUniqueSeriesNames(db, '', invoiceType);
 
     return res.json({ seriesNames });
